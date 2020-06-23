2c2630e89108a186f81b2e59bb4bbf080eeebf91
Hsu Karinsu <m80126colin@gmail.com>
2020-06-23 16:15:07 +0800

    Updated

--- components/Paginator.vue
+++ components/Paginator.vue
@@ -1,15 +1,15 @@
 <template>
-<div class="ui basic buttons">
-  <template v-if="value - 2 > 0">
-    <div class="ui button" v-for="i in Math.min(2, value - 2)" :key="i" @click="toPage(i)">{{ i }}</div>
+<div class="ui mini compact basic buttons">
+  <template v-if="value - item > 0">
+    <div class="ui button" v-for="i in Math.min(item, value - 2)" :key="i" @click="toPage(i)">{{ i }}</div>
   </template>
-  <div class="ui disabled icon button" v-if="value - 4 > 0">
+  <div class="ui disabled icon button" v-if="value - item - 2 > 0">
     <i class="ellipsis horizontal icon"></i>
   </div>
   <div class="ui button" v-if="value - 1 > 0" :key="value - 1" @click="toPage(value - 1)">{{ value - 1 }}</div>
   <div class="ui disabled button">{{ value }}</div>
   <div class="ui button" v-if="value + 1 <= countPage" :key="value + 1" @click="toPage(value + 1)">{{ value + 1 }}</div>
-  <div class="ui disabled icon button" v-if="countPage - value - 3 > 0">
+  <div class="ui disabled icon button" v-if="countPage - value - item - 1 > 0">
     <i class="ellipsis horizontal icon"></i>
   </div>
   <template v-if="countPage - value - 1 > 0">
@@ -22,12 +22,17 @@
 export default {
   name: 'pagingator',
   props: [ 'countPage', 'value' ],
+  data() {
+    return {
+      item: 1
+    }
+  },
   computed: {
     tailPage() {
       const app = this
-      if (app.countPage - app.value - 1 <= 0)
+      if (app.countPage - app.value - app.item + 1 <= 0)
         return []
-      const num = Math.min(2, app.countPage - app.value - 1)
+      const num = Math.min(app.item, app.countPage - app.value - app.item + 1)
       return _.map(_.range(num), i => app.countPage + i + 1 - num)
     }
   },
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: '/howcaloid/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'HOW 音未來 | Howcaloid'
+    }
+  }
+}
