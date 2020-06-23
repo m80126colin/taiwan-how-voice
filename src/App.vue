dd77957fef70ca0ae02c7ffba7053360775463c1
Hsu Karinsu <m80126colin@gmail.com>
2020-06-23 18:21:08 +0800

    Updated sort methods

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
 <div id="app" class="ui container">
   <div class="ui center aligned basic segment">
+    <div class="ui compact basic buttons">
+      <div v-for="(name, value) in sortName" :key="value"
+        @click="sortMethods = value"
+        :class="`ui button ${sortMethods === value ? 'disabled' : ''}`">{{ name }}</div>
+    </div>
+  </div>
+  <div class="ui center aligned basic segment">
     <Paginator v-model="currentPage" :countPage="countPage"></Paginator>
   </div>
   <div class="ui doubling stackable four column grid">
@@ -22,14 +29,45 @@ import axios from 'axios';
 import Card from '../components/Card';
 import Paginator from '../components/Paginator';
 
+const songOrder = (a, b) => (a.song > b.song ? 1 : 0) - (a.song < b.song ? 1 : 0)
+
+const sortName = {
+  'New to Old': '由新到舊',
+  'Old to New': '由舊到新',
+  'Name': '相同歌名',
+  //'View': '觀看次數',
+  //'View Diff': '上升最快'
+}
+
+const sortFuncs = {
+  'New to Old': (a, b) => {
+    if (a.timestamp !== b.timestamp)
+      return b.timestamp - a.timestamp
+    return songOrder(a, b)
+  },
+  'Old to New': (a, b) => {
+    if (a.timestamp !== b.timestamp)
+      return a.timestamp - b.timestamp
+    return songOrder(a, b)
+  },
+  'Name': (a, b) => {
+    if (a.song !== b.song)
+      return songOrder(a, b)
+    return a.timestamp - b.timestamp
+  }
+}
+
 export default {
   name: 'App',
   components: { Card, Paginator },
   data() {
     return {
+      sortName,
+      sortFuncs,
+      sortMethods: 'New to Old',
       pageIndex: 0,
       perPage: 12,
-      list: []
+      originalList: []
     }
   },
   computed: {
@@ -45,26 +83,22 @@ export default {
       const app = this
       const start = app.pageIndex * app.perPage
       return app.list.slice(start, start + app.perPage)
+    },
+    list() {
+      const app = this
+      app.pageIndex = 0
+      return _.chain(app.originalList)
+        .sort(app.sortFuncs[app.sortMethods])
+        .value()
     }
   },
   async mounted() {
     const app = this
     const { data } = await axios.get('videos.json')
-    const list = _.chain(data)
-      .map(row => _.merge(row, {
+    const list = _.map(data, row => _.merge(row, {
         timestamp: (new Date(`${row.date} 00:00:00`)).getTime()
       }))
-      .sort((a, b) => {
-        if (a.timestamp !== b.timestamp)
-          return b.timestamp - a.timestamp
-        if (a.song < b.song)
-          return -1;
-        if (a.song > b.song)
-          return 1;
-        return 0;
-      })
-      .value()
-    Vue.set(app, 'list', list)
+    Vue.set(app, 'originalList', list)
   }
 }
 </script>
